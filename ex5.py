b3ccf54a145117c2985a9edad59a184ddc256489
Sheyma Fathy <exporter.iat@yahoo.com>
2018-01-25 02:26:18 +0200

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,15 @@
+# - - coding: utf- 8 - -
+Name = 'Farieda'#My lovely daughter
+Age = 34 #years
+Hieght= 175 #cm
+Country = 'Iraq' 
+Eyes_color = 'gray'
+Hair_color = 'Brown'
+
+
+print "Let's talk about %s." % Name
+print "Actually that's not too heavy."
+print "She is Egyptian but her mother from %r." % Country
+print "She's got %s eyes and %s hair." % (Eyes_color, Hair_color)
+# this line is tricky, try to get it exactly right
+print "If I add %d, and %d I get %d." % (Age, Hieght, Age + Hieght)
